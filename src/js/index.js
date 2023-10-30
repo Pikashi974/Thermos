16cf15c8fce77a0817c8796a9d3910d8654aab44
Jérémy PAYET <the_big_boss97437@hotmail.fr>
2023-10-30 10:47:00 +0400

    Ajout du cadre pour la température

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,39 @@
-async function init() {}
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+};
+
+let crd;
+
+async function init() {
+  crd = navigator.geolocation.getCurrentPosition(success, error, options);
+  if (crd) {
+    getThermos(crd.latitude, crd.longitude);
+  }
+}
 
 init();
+
+async function getThermos(latitude, longitude) {
+  var object = await fetch(
+    "https://weather.contrateumdev.com.br/api/weather?lat=" +
+      latitude +
+      "&lon=" +
+      longitude
+  ).then((response) => {
+    return response.json();
+  });
+  document.querySelector(".card-title").innerHTML = object.main.temp
+    ? object.main.temp + "°C"
+    : "Erreur, géolocalisation impossible";
+  console.log(object);
+}
+
+function success(pos) {
+  return pos.coords;
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
